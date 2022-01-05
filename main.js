1f1e122074edb0247924b4a502d00755123ad8a0
cleargoaldigital <cleargoal.ng@gmail.com>
2022-01-05 15:33:13 +0100

    updated files

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const btn = document.getElementById("btn");
-const comment = document.getElementById("comments");
-const restart = document.getElementById("reBtn");
+const btn = document.querySelector("#btn");
+const comment = document.querySelector("#comments");
+const restart = document.querySelector("#reBtn");
 
 let count = 0;
 const commentsArray = [
@@ -13,24 +13,28 @@ const commentsArray = [
 btn.addEventListener("click", () => {
   count++;
   if (count < 10) {
-    comment.innerHTML = `${commentsArray[0]} Your click count is:  ${count} times.`;
+    comment.textContent = `${commentsArray[0]} Your click count is:  ${count} times.`;
   } else if (count >= 10 && count < 15) {
-    comment.innerHTML = `${commentsArray[1]} Your count is currently ${count} times.`;
+    comment.textContent = `${commentsArray[1]} Your count is currently ${count} times.`;
   } else if (count >= 15 && count < 20) {
-    comment.innerHTML = `${commentsArray[2]} Now, it's ${count} times.`;
+    comment.textContent = `${commentsArray[2]} Now, it's ${count} times.`;
   } else {
-    comment.innerHTML = `${commentsArray[3]} But why? It's now ${count} times.`;
+    comment.textContent = `${commentsArray[3]} But why? It's now ${count} times.`;
   }
 });
 
 restart.addEventListener("click", () => {
-  comment.innerHTML = "";
+  comment.textContent = "";
   count = 0;
 });
+
 
 // alternative way to clear content
 
 // function ClearFields() {
-//      comment.innerHTML = " ";
+//      comment.textContent = " ";
 //   count = 0;
 // };
+
+// More from TheOdinProject
+
